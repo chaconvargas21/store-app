69166c428317b6b970fce9b7d803618d8fd57093
chaconvargas21 <chaconvargas21@gmail.com>
2024-10-06 22:07:58 -0500

    feat: env prod baseUrl changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: 'https://cursed-app.herokuapp.com',
+  baseUrl: 'http://localhost:4000/api',
   stripe_pk: 'pk_test_51LMwkBFdDJuF2IyCiG5BsxVZsd2xzrV2bwqqXzxU1Jx3Odub5qsRle23KUcWFO1OYvndroJ8Gj4LDNVBDD0NtDcJ00b9TLbUO3',
 };
