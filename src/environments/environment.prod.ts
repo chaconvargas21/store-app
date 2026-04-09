c2e927b1feb069fe3f90bab62d135eb27183cee0
chaconvargas21 <chaconvargas21@gmail.com>
2026-04-08 22:53:58 -0500

    feat: env prod baseUrl new

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: 'https://node-store-back-79996400978.us-central1.run.app/api',
+  baseUrl: 'https://cloudrun-service-79996400978.us-central1.run.app',
   stripe_pk: 'pk_test_51LMwkBFdDJuF2IyCiG5BsxVZsd2xzrV2bwqqXzxU1Jx3Odub5qsRle23KUcWFO1OYvndroJ8Gj4LDNVBDD0NtDcJ00b9TLbUO3',
 };
